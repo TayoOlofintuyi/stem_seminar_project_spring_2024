d0f33ed4411a9f1c12ea8757f66d141d69763cad
Tayo Olofintuyi <tolofi152@gmail.com>
2024-03-27 16:31:39 -0400

    given the bulletin with a dictionary, courseList {course: priority}, and takenCourses, return a schedule that is within credit hours and has the highest priority courses. (ignore pursuingGrad and csStudent for now until it becomes relevant)

---
+++ ScheduleBuilder.js
@@ -0,0 +1,16 @@
+class ScheduleBuilder {
+  constructor() {
+    
+  }
+
+  build(bulletin, takenCourses = [], creditHourMin = 12, creditHourMax = 18, pursuingGrad = false, csStudent = true) {
+
+    let schedule = new Schedule();
+
+    // given the bulletin with a dictionary, courseList {course: priority}, and takenCourses, return a schedule that is within credit hours and has the highest priority courses 
+
+    // (ignore pursuingGrad and csStudent for now until it becomes relevant)
+
+
+  }
+}
